30a1a11f35d322802ad074a67792e481771c95d4
Loginov Ilya <dantes2104@gmail.com>
2019-02-20 17:03:44 +0400

    Adding license

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,3 +1,8 @@
+/*
+Copyright © 2019 Ilya Loginov. All rights reserved.
+Please email dantes2104@gmail.com if you would like permission to do something with the contents of this repository
+*/
+
 import ru.liveproduction.livelib.math.ExpressionObject;
 
 public class TestMain {
