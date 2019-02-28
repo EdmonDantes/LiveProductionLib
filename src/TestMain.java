336f6e7c52863f6886246e04748a40718b04215b
Loginov Ilya <dantes2104@gmail.com>
2019-03-01 00:07:25 +0400

    Add FN1 algorithm

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,10 +4,18 @@ Please email dantes2104@gmail.com if you would like permission to do something w
 */
 
 import ru.liveproduction.livelib.math.ExpressionObject;
+import ru.liveproduction.livelib.math.HashingFNV1;
+
+import static ru.liveproduction.livelib.math.HashingFNV1.Bits.*;
 
 public class TestMain {
 
-    public static void main(String[] args) throws ExpressionObject.WrongCountOperationArgumentsException, ExpressionObject.WrongOperationPriority {
+    public static void main(String[] args) throws Exception {
+        var start = System.currentTimeMillis();
+        System.out.println(HashingFNV1.getInstance(x512).hashString("3qtVZhl1t3jKXJNGE3noXL9gguUWZW3ge28FA9RBYHEXNVFhZjLlJDYcp0MncR9CyvqSAXtgjsffOAQYWFIJyOm5K4GV1yxMJLWesPTR9XQx2AQFEjak6PRzbOHwtuKczaEeIm9LXBIFqJWFR3pMiwo2AgzUj6MOKT3EUTFtbB4yd9NnVKHO95PKkEJvLOI1oJaHACytaQoonUrMiY1UUKjEOHfkVXKH1b5oLfGrGPyoRmgJbsF7nqVpEtmKyhrtkxC0eW45X3YSbvvtUu5LZ8jt7fNO5Pt88T88oqKTQ2aMjUherhFsRnrpPkYRSjvxNf4523Kbv0tWWQH5Xiktvu9ugMBanPKCKjsboyAQlXjjRIlBB3tP9QWMd0Hb0LUGXbtRUg3dLGbiGlie").toString().length()); //93051914
+        var end = System.currentTimeMillis();
+
+        System.out.println(end - start);
         System.out.println("---------------");
     }
 }
