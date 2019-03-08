fe198c219fb05020853555b4b98924cbba187983
Loginov Ilya <dantes2104@gmail.com>
2019-03-09 00:48:32 +0400

    Some reformat code

--- src/ru/liveproduction/livelib/math/expression/EquationObject.java
+++ src/ru/liveproduction/livelib/math/expression/EquationObject.java
@@ -1,7 +1,7 @@
-///*
-//Copyright © 2019 Ilya Loginov. All rights reserved.
-//Please email dantes2104@gmail.com if you would like permission to do something with the contents of this repository
-//*/
+/*
+Copyright © 2019 Ilya Loginov. All rights reserved.
+Please email dantes2104@gmail.com if you would like permission to do something with the contents of this repository
+*/
 
 package ru.liveproduction.livelib.math.expression;
 
--- src/ru/liveproduction/livelib/math/expression/WrongExpressionException.java
+++ src/ru/liveproduction/livelib/math/expression/WrongExpressionException.java
@@ -1,3 +1,8 @@
+/*
+Copyright © 2019 Ilya Loginov. All rights reserved.
+Please email dantes2104@gmail.com if you would like permission to do something with the contents of this repository
+*/
+
 package ru.liveproduction.livelib.math.expression;
 
 public class WrongExpressionException extends Exception {
